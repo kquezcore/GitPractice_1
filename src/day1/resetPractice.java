ee5e52d264bf683df4ebbf91ba5cd37810769aa5
karina quezada <quezadacore32@gmail.com>
2021-07-26 12:52:58 -0400

    test case is all done

--- src/day1/resetPractice.java
+++ src/day1/resetPractice.java
@@ -4,5 +4,9 @@ public class resetPractice {
     // Test case part1 is done
     // Test case part2 is done
     // Test case part3 is done
+    // Test case part4 is done
+    // Test case part5 is done
+// mixed/ soft  reset remove then commit from history but keep the local changes
+// did part 4 part 5 and I am done in one commit
 
 }
