e53f7baaa7b9a74b607d379e18a87615bd8149f8
karina quezada <quezadacore32@gmail.com>
2021-07-30 10:50:17 -0400

    master collection work is done

--- src/day4/conflict.java
+++ src/day4/conflict.java
@@ -7,4 +7,5 @@ public class conflict {
     // and it will become final merge resul
     // this is the collection branch content
 
+    //This is the change collection2 branch does not know about
 }
